194e7e95615711a7ffda112a8592f689bca8559e
Evginia Klementeva <102163438+KlementevaE@users.noreply.github.com>
2022-07-30 17:18:14 +0300

    Собранный проект(недоработанный)
    * Все функции части два реализованы v1
    
    * Созданы сериализаторы и вьюсеты для моделейCategory и Genre(без permissions)
    
    * Исправлены вюсеты на вью-классы для Категорий и Жанров
    
    * Изменен сериализатор для Title
    
    * Убраны лишние строки из тестов
    
    * Реализованы функции part1 v1
    
    * Добавлен функционал confirmation_code, эндпоинты Users
    
    * Добавлены ресурсы auth и users. Изменён файл загрузки данных в БД
    
    * Добавлены ресурсы auth и users. Изменён файл загрузки данных в БД
    
    * Все функции части три реализованы v1
    
    * На review работают get-запросы
    
    * Get-запросы на комментарии работают
    
    * Заполнил README.md
    
    Co-authored-by: Rustam Magomedov <dogarik2007@yandex.ru>
    
    Co-authored-by: novikovps <102223761+novikovps@users.noreply.github.com>
    Co-authored-by: Rustam Magomedov <dogarik2007@yandex.ru>

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,95 @@
+from random import choice
+from django.core.mail import send_mail
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
-from reviews.models import Category, Genre, Title
+from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
+from reviews.models import (User, ROLE_CHOICES, Category,
+                            Genre, Title, Review, Comment)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели user."""
+
+    role = serializers.ChoiceField(choices=ROLE_CHOICES, default="user")
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name",
+                  "last_name", "bio", "role"
+                  )
+
+
+class UserMeSerializer(serializers.ModelSerializer):
+    """Сериализатор для своей учётной записи users/me."""
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name",
+                  "last_name", "bio", "role"
+                  )
+        read_only_fields = ('username', "email", "role")
+
+
+class AuthSignupSerializer(serializers.ModelSerializer):
+    """Сериализатор для самостоятельной регистрации.
+    И получения кода подтверждения"""
+
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    confirmation_code = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "confirmation_code")
+
+    def to_representation(self, instance):
+        return {
+            'username': instance.username,
+            'email': instance.email
+        }
+
+    def validate(self, data):
+        if data['username'] == 'me':
+            raise serializers.ValidationError(
+                'Имя пользователя не может ME!')
+        return data
+
+    def generate_confirmation_code(self):
+        pool = "1234567890"
+        random_str = []
+        for _ in range(4):
+            random_str.append(choice(pool))
+        return "".join(random_str)
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        code = self.generate_confirmation_code()
+        user.confirmation_code = code
+        user.save()
+        to_email = []
+        to_email.append(validated_data["email"])
+        send_mail(
+            subject='YaMDB confirmation code',
+            message=f' Your confirmation code is: {code}.',
+            from_email='yamdb@yamdb.fake',
+            recipient_list=to_email,
+            fail_silently=False
+        )
+        return user
+
+
+class AuthTokenSerializer(serializers.ModelSerializer):
+    """Сериализатор для получения access-токена."""
+
+    username = serializers.CharField(write_only=True)
+    confirmation_code = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "confirmation_code")
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -98,3 +187,46 @@ class TitleCreateUpdateSerializer(serializers.ModelSerializer):
             i += 1
         genres = representation['genre']
         return representation
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True,
+    )
+    author = serializers.SlugRelatedField(
+        default=serializers.CurrentUserDefault(),
+        slug_field='username',
+        read_only=True
+    )
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if request.method == 'POST':
+            if Review.objects.filter(title=title, author=author).exists():
+                raise ValidationError('Вы не можете добавить более'
+                                      'одного отзыва на произведение')
+        return data
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    review = serializers.SlugRelatedField(
+        slug_field='text',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,17 +1,24 @@
 from django.urls import include, path
-from rest_framework.routers import SimpleRouter
+from rest_framework import routers
 
-from .views import (CategoryList, CategoryDetail, GenreList, GenreDetail,
-                    TitleViewSet)
+import api.views as views
 
-router_v1 = SimpleRouter()
-router_v1.register(r'titles', TitleViewSet)
-
+router_v1 = routers.DefaultRouter()
+router_v1.register(r'users', views.UserViewSet, basename="users")
+router_v1.register(r'titles', views.TitleViewSet)
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
+                   views.ReviewViewSet, basename='reviews')
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
+                   r'/comments', views.CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    path('v1/categories/', CategoryList.as_view()),
-    path('v1/categories/<slug:slug>/', CategoryDetail.as_view()),
-    path('v1/genres/', GenreList.as_view()),
-    path('v1/genres/<slug:slug>/', GenreDetail.as_view()),
+    path('v1/auth/signup/', views.AuthSignupView.as_view()),
+    path('v1/auth/token/',
+         views.AuthTokenView.as_view(), name='token_obtain_pair'
+         ),
+    path('v1/categories/', views.CategoryList.as_view()),
+    path('v1/categories/<slug:slug>/', views.CategoryDetail.as_view()),
+    path('v1/genres/', views.GenreList.as_view()),
+    path('v1/genres/<slug:slug>/', views.GenreDetail.as_view()),
 ]
