c3940341cea8f2034ae1b01dd10ce92bff1d9188
novikovps <102223761+novikovps@users.noreply.github.com>
2022-07-30 20:51:18 +0700

    Все функции части три реализованы v1
    * На review работают get-запросы
    
    * Get-запросы на комментарии работают
    
    * Заполнил README.md
    
    Co-authored-by: Rustam Magomedov <dogarik2007@yandex.ru>

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,6 +6,10 @@ import api.views as views
 router_v1 = routers.DefaultRouter()
 router_v1.register(r'users', views.UserViewSet, basename="users")
 router_v1.register(r'titles', views.TitleViewSet)
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
+                   views.ReviewViewSet, basename='reviews')
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
+                   r'/comments', views.CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from rest_framework.generics import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import (response, status,
                             viewsets,
@@ -11,11 +11,12 @@ from rest_framework_simplejwt.tokens import AccessToken
 from .serializers import (UserSerializer, UserMeSerializer,
                           AuthSignupSerializer, AuthTokenSerializer,
                           CategorySerializer, GenreSerializer,
-                          TitleReadSerializer, TitleCreateUpdateSerializer)
-from .permissions import AdminPermission  # , SelfPermission
+                          TitleReadSerializer, TitleCreateUpdateSerializer,
+                          ReviewSerializer, CommentSerializer)
+from .permissions import AdminPermission, IsAdminModeratorOwnerOrReadOnly
 from .pagination import UserPagination
 from .filters import TitleFilter
-from reviews.models import User, Category, Genre, Title
+from reviews.models import User, Category, Genre, Title, Review
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -129,3 +130,32 @@ class TitleViewSet(viewsets.ModelViewSet):
         if method == 'GET':
             return TitleReadSerializer
         return TitleCreateUpdateSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAdminModeratorOwnerOrReadOnly]
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get("title_id"))
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAdminModeratorOwnerOrReadOnly]
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id, title=title_id)
+        serializer.save(author=self.request.user, review=review)
