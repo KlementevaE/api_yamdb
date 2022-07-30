bfdafb93bd45f8954dbc3f232666bfb00e58dafc
novikovps <102223761+novikovps@users.noreply.github.com>
2022-07-30 19:58:03 +0700

    Реализованы функции part1 v1
    * Добавлен функционал confirmation_code, эндпоинты Users
    
    * Добавлены ресурсы auth и users. Изменён файл загрузки данных в БД
    
    * Добавлены ресурсы auth и users. Изменён файл загрузки данных в БД

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,93 @@
+from random import choice
+from django.core.mail import send_mail
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
-from reviews.models import Category, Genre, Title
+from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
+
+from reviews.models import User, ROLE_CHOICES, Category, Genre, Title
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели user."""
+
+    role = serializers.ChoiceField(choices=ROLE_CHOICES, default="user")
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name",
+                  "last_name", "bio", "role"
+                  )
+
+
+class UserMeSerializer(serializers.ModelSerializer):
+    """Сериализатор для своей учётной записи users/me."""
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name",
+                  "last_name", "bio", "role"
+                  )
+        read_only_fields = ('username', "email", "role")
+
+
+class AuthSignupSerializer(serializers.ModelSerializer):
+    """Сериализатор для самостоятельной регистрации.
+    И получения кода подтверждения"""
+
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    confirmation_code = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "confirmation_code")
+
+    def to_representation(self, instance):
+        return {
+            'username': instance.username,
+            'email': instance.email
+        }
+
+    def validate(self, data):
+        if data['username'] == 'me':
+            raise serializers.ValidationError(
+                'Имя пользователя не может ME!')
+        return data
+
+    def generate_confirmation_code(self):
+        pool = "1234567890"
+        random_str = []
+        for _ in range(4):
+            random_str.append(choice(pool))
+        return "".join(random_str)
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        code = self.generate_confirmation_code()
+        user.confirmation_code = code
+        user.save()
+        to_email = []
+        to_email.append(validated_data["email"])
+        send_mail(
+            subject='YaMDB confirmation code',
+            message=f' Your confirmation code is: {code}.',
+            from_email='yamdb@yamdb.fake',
+            recipient_list=to_email,
+            fail_silently=False
+        )
+        return user
+
+
+class AuthTokenSerializer(serializers.ModelSerializer):
+    """Сериализатор для получения access-токена."""
+
+    username = serializers.CharField(write_only=True)
+    confirmation_code = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "confirmation_code")
 
 
 class CategorySerializer(serializers.ModelSerializer):
--- api_yamdb/reviews/management/commands/import_data.py
+++ api_yamdb/reviews/management/commands/import_data.py
@@ -1,87 +1,52 @@
 from django.core.management.base import BaseCommand
 
 import csv
+import re
 
-from reviews.models import (User, Category, Genre, Title, TitleGenre,
-                            Review, Comment)
+from api_yamdb.settings import BASE_DIR
+from reviews.models import (
+    User, Category,
+    Genre, Title,
+    TitleGenre, Review, Comment
+)
 
-import os
 
-DIRECTORY = os.getcwd() + '/static/data/'
+STATIC_DATA = BASE_DIR + r'\static\data'
+
+CATEGORY_FILE = STATIC_DATA + r'\category.csv'
+GENRE_FILE = STATIC_DATA + r'\genre.csv'
+USERS_FILE = STATIC_DATA + r'\users.csv'
+TITLES_FILE = STATIC_DATA + r'\titles.csv'
+GENRE_TITLE_FILE = STATIC_DATA + r'\genre_title.csv'
+REVIEW_FILE = STATIC_DATA + r'\review.csv'
+COMMENT_FILE = STATIC_DATA + r'\comments.csv'
+
+
+FILES = {
+    Category: CATEGORY_FILE,
+    Genre: GENRE_FILE,
+    User: USERS_FILE,
+    Title: TITLES_FILE,
+    TitleGenre: GENRE_TITLE_FILE,
+    Review: REVIEW_FILE,
+    Comment: COMMENT_FILE,
+}
+
+
+def read_from_csv(model, datafile):
+    with open(datafile, 'r', encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            # Две строки с re.sub нужны в связи с тем,
+            # что в csv-файлах не везде корректно названы стобцы
+            row = {re.sub(r'(category)$',
+                          'category_id', k): v for (k, v) in row.items()}
+            row = {re.sub(r'(author)$',
+                          'author_id', k): v for (k, v) in row.items()}
+            _, created = model.objects.get_or_create(**row)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        with open(DIRECTORY + '/category.csv') as f:
-            reader = csv.reader(f)
-            next(reader, None)
-            for row in reader:
-                _, created = Category.objects.get_or_create(
-                    id=row[0],
-                    name=row[1],
-                    slug=row[2],
-                )
-        with open(DIRECTORY + '/genre.csv') as f:
-            reader = csv.reader(f)
-            next(reader, None)
-            for row in reader:
-                _, created = Genre.objects.get_or_create(
-                    id=row[0],
-                    name=row[1],
-                    slug=row[2],
-                )
-        with open(DIRECTORY + '/users.csv') as f:
-            reader = csv.reader(f)
-            next(reader, None)
-            for row in reader:
-                _, created = User.objects.get_or_create(
-                    id=row[0],
-                    username=row[1],
-                    email=row[2],
-                    role=row[3],
-                    bio=row[4],
-                    first_name=row[5],
-                    last_name=row[6],
-                )
-        with open(DIRECTORY + '/titles.csv') as f:
-            reader = csv.reader(f)
-            next(reader, None)
-            for row in reader:
-                _, created = Title.objects.get_or_create(
-                    id=row[0],
-                    name=row[1],
-                    year=row[2],
-                    category_id=row[3],
-                )
-        with open(DIRECTORY + '/genre_title.csv') as f:
-            reader = csv.reader(f)
-            next(reader, None)
-            for row in reader:
-                _, created = TitleGenre.objects.get_or_create(
-                    id=row[0],
-                    title_id=row[1],
-                    genre_id=row[2],
-                )
-        with open(DIRECTORY + '/review.csv') as f:
-            reader = csv.reader(f)
-            next(reader, None)
-            for row in reader:
-                _, created = Review.objects.get_or_create(
-                    id=row[0],
-                    title_id=row[1],
-                    text=row[2],
-                    author_id=row[3],
-                    score=row[4],
-                    pub_date=row[5],
-                )
-        with open(DIRECTORY + '/comments.csv') as f:
-            reader = csv.reader(f)
-            next(reader, None)
-            for row in reader:
-                _, created = Comment.objects.get_or_create(
-                    id=row[0],
-                    review_id=row[1],
-                    text=row[2],
-                    author_id=row[3],
-                    pub_date=row[4],
-                )
+        for model, datafile in FILES.items():
+            read_from_csv(model, datafile)
