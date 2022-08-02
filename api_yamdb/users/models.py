62f17887b21d3cd2578d90c35f4873cb13607926
Evginia Klementeva <102163438+KlementevaE@users.noreply.github.com>
2022-08-02 04:09:36 +0300

    Update models.py

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -28,6 +28,7 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+        ordering = ['-pk']
 
     def __str__(self):
         return self.username
